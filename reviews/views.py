7cbe31945263018cf00754cab8e5c98f00b19247
aqe-gl <filatovnick11@gmail.com>
2025-10-21 15:58:59 +0100

    transfer data between frontend-backend with GET method

--- reviews/views.py
+++ reviews/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def reviews(request):
-    return render(request, "reviews.html")
+    name1 = "nick"
+    email1 = "nick@gamil.com"
+    review1 = "Nice View"
+
+    name2 = request.GET.get("name")
+    email2 = request.GET.get("email")
+    review2 = request.GET.get("review")
+
+    return render(request, "reviews.html", {'name1': name1, 'email1': email1, 'review1': review1, 'name2': name2, 'email2': email2, 'review2': review2})
