5faed6e8638d2261cfe0e4162745a2766b9278ca
aqe-gl <filatovnick11@gmail.com>
2025-12-18 18:48:10 +0000

    added 'created at' field, added styles

--- reviews/forms.py
+++ reviews/forms.py
@@ -7,4 +7,16 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['name', 'email', 'review', 'rating']
-        widgets = {'rating': forms.NumberInput(attrs={'min': 1, 'max': 10})}
+        widgets = {
+            'rating': forms.NumberInput(attrs={
+                'min': 1,
+                'max': 10,
+                'placeholder': ' '
+            }),
+            'name': forms.TextInput(attrs={'placeholder': ' '}),
+            'email': forms.EmailInput(attrs={'placeholder': ' '}),
+            'review': forms.Textarea(attrs={
+                'placeholder': ' ',
+                'rows': 4
+            }),
+        }
--- reviews/models.py
+++ reviews/models.py
@@ -1,12 +1,12 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
 class Review(models.Model):
     name = models.CharField(max_length=25)
     email = models.EmailField()
     review = models.TextField()
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name} | {self.rating} | {self.review}"
